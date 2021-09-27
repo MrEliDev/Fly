e896051bdc29722b12175cb78c78ee27af8103c8
MrEliDev <87885297+MrEliDev@users.noreply.github.com>
2021-09-26 19:51:48 -0500

    main.php

---
+++ main.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace: Fly;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\event\Listener;
+// Player
+use pocketmine\Player;
+
+// Clase
+class main extends PluginBase
